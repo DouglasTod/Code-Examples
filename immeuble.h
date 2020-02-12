3c999e127289089c88f50bfc60c5aa18d31d4958
DouglasTod <60976194+DouglasTod@users.noreply.github.com>
2020-02-12 22:46:38 +0100

    Add files via upload

---
+++ immeuble.h
@@ -0,0 +1,13 @@
+typedef struct
+    {
+        float hauteur;
+        float largeur;
+        float longueur;
+        int nbEtages;
+        int nbAppart;
+    } Immeuble;
+
+void agrandirImmeuble(Immeuble,Immeuble*);
+
+void afficherValeursChampsImmeuble(Immeuble);
+
